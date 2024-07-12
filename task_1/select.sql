a900751cee4f6a8206fa146a0f1658163a6fd733
Crystweb <remont.moy-pc@yandex.ru>
2024-07-12 23:05:34 +0300

    add task 2

--- task_1/select.sql
+++ task_1/select.sql
@@ -1,17 +1,17 @@
 -- 1. Отримати всі завдання певного користувача
 SELECT *
 FROM tasks
-WHERE user_id = 1;
+WHERE user_id = 25;
 
 -- 2. Вибрати завдання за певним статусом
 SELECT *
 FROM tasks
-WHERE status_id = (SELECT id FROM status WHERE name = 'new');
+WHERE status_id = (SELECT id FROM status WHERE name = 'in review');
 
 -- 3. Оновити статус конкретного завдання
 UPDATE tasks
-SET status_id = (SELECT id FROM status WHERE name = 'in progress')
-WHERE id = 1;
+SET status_id = (SELECT id FROM status WHERE name = 'completed')
+WHERE id = 138;
 
 -- 4. Отримати список користувачів, які не мають жодного завдання
 SELECT *
@@ -20,7 +20,7 @@ WHERE id NOT IN (SELECT user_id FROM tasks);
 
 -- 5. Додати нове завдання для конкретного користувача
 INSERT INTO tasks (title, description, status_id, user_id)
-VALUES ('Назва завдання', 'Опис завдання', (SELECT id FROM status WHERE name = 'new'), 1);
+VALUES ('Тестове завдання №1', 'Опис тестового завдання №1', (SELECT id FROM status WHERE name = 'assigned'), 19);
 
 -- 6. Отримати всі завдання, які ще не завершено
 SELECT *
@@ -30,17 +30,17 @@ WHERE status_id <> (SELECT id FROM status WHERE name = 'completed');
 -- 7. Видалити конкретне завдання
 DELETE
 FROM tasks
-WHERE id = 1;
+WHERE id = 127;
 
--- 8. Знайти користувачів з певною електронною поштою
+-- 8. Оновити ім'я користувача та пошту
+UPDATE users
+SET name = "Ihor Rukavitsyn", email = "ihor@goit.com"
+WHERE id = 18;
+
+-- 9. Знайти користувачів з певною електронною поштою
 SELECT *
 FROM users
-WHERE email LIKE '%@example.com';
-
--- 9. Оновити ім'я користувача
-UPDATE users
-SET fullname = "Нове ім'я"
-WHERE id = 1;
+WHERE email LIKE '%@goit.com';
 
 -- 10. Отримати кількість завдань для кожного статусу
 SELECT status.name, COUNT(tasks.id)
@@ -52,7 +52,7 @@ GROUP BY status.name;
 SELECT tasks.*
 FROM tasks
 JOIN users ON tasks.user_id = users.id
-WHERE users.email LIKE '%@example.com';
+WHERE users.email LIKE '%@goit.com';
 
 -- 12. Отримати список завдань, що не мають опису
 SELECT *
@@ -61,13 +61,13 @@ WHERE description IS NULL
    OR description = '';
 
 -- 13. Вибрати користувачів та їхні завдання, які є у статусі "в процесі"
-SELECT users.fullname, tasks.title
+SELECT users.name, tasks.title
 FROM users
 INNER JOIN tasks ON users.id = tasks.user_id
 WHERE tasks.status_id = (SELECT id FROM status WHERE name = 'in progress');
 
 -- 14. Отримати користувачів та кількість їхніх завдань
-SELECT users.fullname, COUNT(tasks.id) AS task_count
+SELECT users.name, COUNT(tasks.id) AS task_count
 FROM users
 LEFT JOIN tasks ON users.id = tasks.user_id
-GROUP BY users.fullname;
+GROUP BY users.name;
